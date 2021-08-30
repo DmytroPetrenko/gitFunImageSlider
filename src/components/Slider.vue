dedca8faaf0c066742e42883a78e5f1e25892210
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-08-30 16:11:27 +0300

    slider with zoom and arrows

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,37 +1,76 @@
 <template>
 	<div class="slider">
 		<div class="mainImage">
-			<zoom-on-hover :img-normal="activeImage" :img-zoom="zoomedActiveImage" />
+			<VueMagnifier :src="activeImage" :src-large="zoomedActiveImage" />
 		</div>
 		<div class="images">
+			<font-awesome-icon
+				:icon="['fas', 'arrow-left']"
+				class="arrow"
+				@click="decreaseActiveImageIndex"
+			/>
 			<img
-				v-for="image in images"
+				v-for="(image, index) in images"
 				:key="image.id"
 				:src="image.thumbnailUrl"
-				@click="setActiveImage(image)"
+				@click="setActiveImageIndex(index)"
+			/>
+			<font-awesome-icon
+				:icon="['fas', 'arrow-right']"
+				class="arrow"
+				@click="increaseActiveImageIndex"
 			/>
 		</div>
 	</div>
 </template>
 
 <script>
+import VueMagnifier from "@/components/VueMagnifier"
 export default {
 	props: {
 		images: {
 			type: Array,
 			required: true,
+			validator(value) {
+				if (value.length > 4) {
+					value = value.splice(4, value.length - 4)
+				}
+				return true
+			},
 		},
 	},
+	components: { VueMagnifier },
 	data() {
 		return {
 			zoomedActiveImage: this.images?.[0]?.url,
 			activeImage: this.images?.[0]?.thumbnailUrl,
+			activeImageIndex: 0,
 		}
 	},
 	methods: {
-		setActiveImage(image) {
-			this.activeImage = image.thumbnailUrl
-			this.zoomedActiveImage = image.url
+		setActiveImage() {
+			this.activeImage = this.images?.[this.activeImageIndex]?.thumbnailUrl
+			this.zoomedActiveImage = this.images?.[this.activeImageIndex]?.url
+		},
+		setActiveImageIndex(index) {
+			this.activeImageIndex = index
+			this.setActiveImage()
+		},
+		decreaseActiveImageIndex() {
+			if (this.activeImageIndex) {
+				this.activeImageIndex--
+			} else {
+				this.activeImageIndex = this.images?.length - 1
+			}
+			this.setActiveImage()
+		},
+		increaseActiveImageIndex() {
+			if (this.activeImageIndex === this.images?.length - 1) {
+				this.activeImageIndex = 0
+			} else {
+				this.activeImageIndex++
+			}
+			this.setActiveImage()
 		},
 	},
 }
@@ -50,7 +89,14 @@ export default {
 }
 .mainImage {
 	margin: 10px;
-	cursor: none;
+	max-width: 400px;
+	align-self: center;
+}
+.arrow {
+	cursor: pointer;
+	font-size: 30px;
+	color: grey;
+	align-self: center;
 }
 .images img {
 	margin: 5px;
